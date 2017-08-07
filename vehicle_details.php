2852f9c78bc7c2d183dc16f0229b9e4660226182
RenatoCabral <carvalho.renato6@gmail.com>
2017-08-07 15:50:23 -0300

    melhoria no layout da página de detalhes dos veiculos

--- vehicle_details.php
+++ vehicle_details.php
@@ -8,28 +8,28 @@
                 <h1>Fiat Stilo Duologic 1.8 Attractive Flex 8v 5p </h1>
                 <p><strong>R$ 30.000,00</strong></p>
             </div>
-            <div class=" col s12 m6 l6 vehicle-details">
+            <div class=" col s12 m12 l6 vehicle-details">
                 <p><i class="material-icons small left vehicle-details-icon">date_range</i><strong>Ano:</strong> 2017/2017</p>
             </div>
-            <div class=" col s12 m6 l6 vehicle-details">
+            <div class=" col s12 m12 l6 vehicle-details">
                 <p><i class="material-icons small left vehicle-details-icon">av_timer</i><strong>KM:</strong> 116000</p>
             </div>
-            <div class=" col s12 m6 l6 vehicle-details">
+            <div class=" col s12 m12 l6 vehicle-details">
                 <p><i class="material-icons small left vehicle-details-icon">color_lens</i><strong>Cor:</strong> Prata</p>
             </div>
-            <div class=" col s12 m6 l6 vehicle-details">
+            <div class=" col s12 m12 l6 vehicle-details">
                 <p><i class="material-icons small left vehicle-details-icon">local_gas_station</i><strong>Portas:</strong> 4</p>
             </div>
-            <div class=" col s12 m6 l6 vehicle-details">
+            <div class=" col s12 m12 l6 vehicle-details">
                 <p><i class="material-icons small left vehicle-details-icon">local_gas_station</i><strong>Combústivel:</strong> Flex</p>
             </div>
-            <div class=" col s12 m6 l6 vehicle-details">
+            <div class=" col s12 m12 l6 vehicle-details">
                 <p><i class="material-icons small left vehicle-details-icon">directions_car</i><strong>Câmbio:</strong>Automático</p>
             </div>
-            <div class=" col s12 m6 l6 vehicle-details">
+            <div class=" col s12 m12 l6 vehicle-details">
                 <p><i class="material-icons small left vehicle-details-icon">directions_car</i><strong>Conservação:</strong> Seminovo</p>
             </div>
-            <div class=" col s12 m6 l6 vehicle-details">
+            <div class=" col s12 m12 l6 vehicle-details">
                 <p><i class="material-icons small left vehicle-details-icon">insert_invitation</i><strong>Final Placa:</strong> 5</p>
             </div>
 
@@ -64,80 +64,186 @@
     </div>
 </div>
 
+<!-- Tabs -->
 <div class="container-fluid">
     <div class="row">
-        <div class="col s12 m12 l12">
+        <div class="col s12 m12 l6">
             <ul id="tabs-swipe-demo" class="tabs vehicle-details-tabs">
-                <li class="tab col s3"><a href="#test-swipe-1">+ Informações</a></li>
-                <li class="tab col s3"><a class="active" href="#test-swipe-2">Obs. Vendedor</a></li>
-                <li class="tab col s3"><a href="#test-swipe-3">Dados do Vendedor</a></li>
+                <li class="tab col s4"><a href="#test-swipe-1">+ Informações</a></li>
+                <li class="tab col s4"><a class="active" href="#test-swipe-2">Obs. Vendedor</a></li>
+                <li class="tab col s4"><a href="#test-swipe-3">Dados do Vendedor</a></li>
             </ul>
-            <div id="test-swipe-1" class="col s12 blue">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque dolores eos eveniet neque nostrum quidem quod ratione ullam ut. Accusantium alias consequatur, debitis fugiat ipsam magni odio quas quod saepe.</div>
-            <div id="test-swipe-2" class="col s12 red">Test 2</div>
-            <div id="test-swipe-3" class="col s12 green">Test 3</div>
-        </div>
-    </div>
-</div>
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col s12 m12 l12">
-            <div class="col s12 m12 l6 red">
-                <h5 class="title_fipe">Envia sua proposta</h5>
-                <div class="row">
-                    <div class="input-field col s12 m12 l12">
-                        <i class="material-icons prefix">account_circle</i>
-                        <input id="nome" type="text" class="validate">
-                        <label for="nome">Nome Completo: </label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s12 m12 l12">
-                        <i class="material-icons prefix">markunread</i>
-                        <input id="email" type="email" class="validate">
-                        <label for="email">E-mail: </label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s12 m12 l12">
-                        <i class="material-icons prefix">phone</i>
-                        <input id="telefone" type="email" class="validate">
-                        <label for="telefone">Telefone: </label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s12 m12 l12">
-                        <i class="material-icons prefix">phone_iphone</i>
-                        <input id="telefone" type="email" class="validate">
-                        <label for="telefone">Celular: </label>
-                    </div>
-                </div>
-                <div class="row">
-                    <form class="col s12">
-                        <div class="row">
-                            <div class="input-field col s12 m6 l12">
-                                <i class="material-icons prefix">comment</i>
-                                <textarea id="textarea1" class="materialize-textarea"></textarea>
-                                <label for="textarea1">Detalhe sua Proposta</label>
-                            </div>
+            <div id="test-swipe-1" class="col s12 m12 l12">
+                <div class="col s12 m12 l12">
+                    <div class="row">
+                        <div class=" col s6 m6 l6 tab-informacoes">
+                            <p><i class="material-icons small left vehicle-details-icon">date_range</i><strong>Tipo:</strong> Sedan</p>
                         </div>
-                    </form>
-                </div>
 
-                <div class="input-field col s12 m6 l6 btn_basic_search">
-                    <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                        <div class=" col s6 m6 l6 tab-informacoes">
+                            <p><i class="material-icons small left vehicle-details-icon">date_range</i><strong>Cód. Veículo:</strong> 123456</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class=" col s6 m6 l6 tab-informacoes">
+                            <p><i class="material-icons small left vehicle-details-icon">date_range</i><strong>Motor:</strong> 2.0</p>
+                        </div>
+                        <div class=" col s6 m6 l6 tab-informacoes">
+                            <p><i class="material-icons small left vehicle-details-icon">date_range</i><strong>Fabricante:</strong> Ford</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class=" col s6 m6 l6 tab-informacoes">
+                            <p><i class="material-icons small left vehicle-details-icon">date_range</i><strong>Modelo:</strong> Fiat Stilo</p>
+                        </div>
+                        <div class=" col s6 m6 l6 tab-informacoes">
+                            <p><i class="material-icons small left vehicle-details-icon">date_range</i><strong>Outro:</strong> outro</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m12 l12 tab-informacoes-opcionais">
+                    <div class="row">
+                        <p class="tab-title-itens-opcionais">Itens e outros opcionais</p>
+                        <form action="#">
+                            <div class="row">
+                                <p class="col s12 m12 l3">
+                                    <input  type="checkbox" id="test6" checked="checked" />
+                                    <label for="test6">Air-Bag</label>
+                                </p>
+                                <p class="col s12 m12 l3">
+                                    <input  type="checkbox" id="test6" checked="checked" />
+                                    <label for="test6">Ar Condicionado</label>
+                                </p>
+                                <p class="col s12 m12 l3">
+                                    <input type="checkbox" id="test6" checked="checked" />
+                                    <label for="test6">Câmera de Ré</label>
+                                </p>
+                                <p class="col s12 m12 l3">
+                                    <input type="checkbox" id="test6" checked="checked" />
+                                    <label for="test6">Direção Hidráulica</label>
+                                </p>
+                            </div>
+                            <div class="row">
+                                <p class="col s12 m12 l3">
+                                    <input  type="checkbox" id="test6" checked="checked" />
+                                    <label for="test6">Farol de Neblina</label>
+                                </p>
+                                <p class="col s12 m12 l3">
+                                    <input  type="checkbox" id="test6" checked="checked" />
+                                    <label for="test6">Freio ABS</label>
+                                </p>
+                                <p class="col s12 m12 l3">
+                                    <input type="checkbox" id="test6" checked="checked" />
+                                    <label for="test6">Vidro Elétrico</label>
+                                </p>
+                                <p class="col s12 m12 l3">
+                                    <input type="checkbox" id="test6" checked="checked" />
+                                    <label for="test6">Retrovisor Elétrico</label>
+                                </p>
+                            </div>
+                            <div class="row">
+                                <p class="col s12 m12 l3">
+                                    <input  type="checkbox" id="test6" checked="checked" />
+                                    <label for="test6">Som</label>
+                                </p>
+                                <p class="col s12 m12 l3">
+                                    <input  type="checkbox" id="test6" checked="checked" />
+                                    <label for="test6">Porta USB</label>
+                                </p>
+                                <p class="col s12 m12 l3">
+                                    <input type="checkbox" id="test6" checked="checked" />
+                                    <label for="test6">Rodas de liga leve</label>
+                                </p>
+                                <p class="col s12 m12 l3">
+                                    <input type="checkbox" id="test6" checked="checked" />
+                                    <label for="test6">Sensor de estacionamento</label>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col s12 m12 l12 tab-informacoes-opcionais">
+                    <div class="row">
+                        <p class="tab-title-itens-opcionais">Obs. do Vendedor</p>
+                    </div>
+                </div>
+            </div>
+            <div id="test-swipe-2" class="col s12 m12 l12 green">
+                <div class="row">
+                    <div class="col s12 m12 l12">Obs. Vendedor</div>
+                </div>
+            </div>
+            <div id="test-swipe-3" class="col s12 m12 l12 blue">Dados do Vendedor</div>
+        </div>
+
+        <!--FORM ENVIO PROPOSTA--->
+
+        <div class="col s12 m12 l6">
+            <h5 class="title_fipe">Envia sua proposta</h5>
+            <div class="row">
+                <div class="input-field col s12 m12 l12">
+                    <i class="material-icons prefix">account_circle</i>
+                    <input id="nome" type="text" class="validate">
+                    <label for="nome">Nome Completo: </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12 m12 l12">
+                    <i class="material-icons prefix">markunread</i>
+                    <input id="email" type="email" class="validate">
+                    <label for="email">E-mail: </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12 m12 l12">
+                    <i class="material-icons prefix">phone</i>
+                    <input id="telefone" type="email" class="validate">
+                    <label for="telefone">Telefone: </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12 m12 l12">
+                    <i class="material-icons prefix">phone_iphone</i>
+                    <input id="telefone" type="email" class="validate">
+                    <label for="telefone">Celular: </label>
+                </div>
+            </div>
+            <div class="row">
+                <form action="#" class="col s12 m12 l12">
+                    <div class="file-field input-field col s12 m12 l12">
+                        <div class="btn">
+                            <span>Arquivo</span>
+                            <input type="file" multiple>
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text" placeholder="Deseja incluir outro veículo no negócio? Anexe fotos.">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="row">
+                <form class="col s12">
+                    <div class="row">
+                        <div class="input-field col s12 m12 l12">
+                            <i class="material-icons prefix">mode_edit</i>
+                            <!--                                <i class="material-icons prefix">comment</i>-->
+                            <textarea id="textarea1" class="materialize-textarea"></textarea>
+                            <label for="textarea1">Detalhe sua Proposta</label>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="row vehicle-details-button-enviar-proposta">
+                <div class="input-field col s12 m12 l12 btn_basic_search">
+                    <button class="btn waves-effect waves-light" type="submit" name="action">Enviar Mensagem
                         <i class="material-icons right">send</i>
                     </button>
                 </div>
             </div>
-            <div class="col s12 m12 l6 blue">
-                <h5 class="title_fipe">Entre em contato</h5>
-
-            </div>
-
         </div>
-
     </div>
 </div>
+
+
 
 <?php include 'footer.php'?>
